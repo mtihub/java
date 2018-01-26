2dd7368302c91488679e33b2d4faa0e27ec57447
Md Tanvirul Islam <md.t.islam@uconn.edu>
2018-01-26 00:27:44 -0500

    javafx

---
+++ javafx/sceneAndWindow/src/sceneAndWindow/Main.java
@@ -0,0 +1,31 @@
+package sceneAndWindow;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Button button = new Button();
+		button.setText("Do you wish to enter the Matrix?");
+		button.setOnAction(e -> OptionsBox.display("Matrix", "Choose your fate, Neo"));
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 400, 300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+}
---
+++ javafx/sceneAndWindow/src/sceneAndWindow/OptionsBox.java
@@ -0,0 +1,43 @@
+package sceneAndWindow;
+
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+
+public class OptionsBox {
+	static boolean choice;
+	
+	public static void display(String title, String message) {
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle(title);
+		
+		Label label = new Label();
+		label.setText(message);
+		
+		Button yes = new Button("YES");
+		Button no  = new Button("NO");
+		
+		yes.setOnAction(e -> {
+			choice = true;
+			window.close();
+		});
+		
+		no.setOnAction(e -> {
+			choice = false;
+			window.close();
+		});
+		
+		VBox layout = new VBox();
+		layout.getChildren().addAll(label, yes, no);
+		layout.setAlignment(Pos.CENTER);
+		
+		Scene scene = new Scene(layout, 250, 200);
+		window.setScene(scene);
+		window.show();
+	}
+
+
+}
